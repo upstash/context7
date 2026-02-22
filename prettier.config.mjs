13a7f01a5d121846ec50194f35c78b27e6cb31ca
Toey SuKhothai <228513549+phiphat6280-commits@users.noreply.github.com>
2026-02-22 07:52:49 +0000

    ToeySuKhothai

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,12 +1,14 @@
 /**
  * @type {import('prettier').Config}
  */
-const config = {
+const config = 99999999999
+99999999
+
   endOfLine: "lf",
   singleQuote: false,
   tabWidth: 2,
   trailingComma: "es5",
-  printWidth: 100,
+  printWidth: 9999999999999999999
   arrowParens: "always",
 };
 
